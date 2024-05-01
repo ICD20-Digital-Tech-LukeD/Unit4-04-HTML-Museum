4eb8d51d97c5e656175b23a6dc2933f4d1ec5758
LukeDiBert <159164688+LukeDiBert@users.noreply.github.com>
2024-05-01 15:00:31 +0000

    co

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function MuseumEntry(){
+  let age = document.getElementById("age").value;
+  let select = document.getElementById("days");
+  let day = select.options[select.selectedIndex].value
+  let result = document.getElementById("result");
+  if (age < 5 || age > 95){
+    result.innerHTML = "You can enter for free";
+  }
+  else if (age >= 12 && age <= 21 || day == "tuesday" || day == "thursday"){
+    result.innerHTML = "You can apply for student discount";
+  }
+  else{
+    result.innerHTML = "You have to pay full price";
+  }
+}
